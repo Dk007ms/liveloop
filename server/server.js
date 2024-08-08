0d7d64b0d699323e318b1713e1ec4e22242a6e6e
Dhruv_Kaushik12 <88232495+Dk007ms@users.noreply.github.com>
2024-08-08 23:11:28 +0530

    adding new server CORS

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
-    origin: process.env.REACT_APP_BASE_URL,
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -21,8 +21,8 @@ const io = socketio(server, {
 // CORS configuration (optional, but necessary if frontend and backend are on different origins)
 app.use(
   cors({
-    origin: process.env.REACT_APP_BASE_URL, // Allow only this origin
-    credentials: true, // Allow cookies and credentials to be sent
+    origin: "*",
+    credentials: true,
   })
 );
 
